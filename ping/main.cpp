3c8f74eaab4e6ffa0c90add4bace03c8da653178
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-07-10 11:40:45 +0100

    Initial Commit

---
+++ ping/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ping
+//
+//  Created by Chidume Nnamdi on 10/07/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
